ad2338bd73580a88f6060d2cbf42102e811819f0
Don Ky <don.d.ky@gmail.com>
2017-07-11 10:17:55 -0400

    Add append function

--- views/views.go
+++ views/views.go
@@ -1,12 +1,31 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
 
 func NewView(layout string, files ...string) *View {
-	files = append(files, 
+	/*
+	files = append(files,
 		"views/layouts/footer.gohtml",
 		"views/layouts/navbar.gohtml",
 		"views/layouts/bootstrap.gohtml")
+	*/
+	files = append(files, layoutFiles()...)
 	//template.ParseFiles("a", "b", "c") ... below "unravels the slice"
 	t, err := template.ParseFiles(files...)
 	if err != nil {
