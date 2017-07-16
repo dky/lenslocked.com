8343670401fbe52e40e9da2808ccf4fdcf6088a5
Don Ky <don.d.ky@gmail.com>
2017-07-15 23:56:41 -0400

    Start work on sign up form

--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
-var contactView *views.View
-var faqsView *views.View
+var (
+	homeView *views.View
+	contactView *views.View
+	faqsView *views.View
+	signupView *views.View
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -46,6 +49,7 @@ func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqsView = views.NewView("bootstrap_improved", "views/faqs.gohtml")
+	signupView = views.NewView("bootstrap", "views/si
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
