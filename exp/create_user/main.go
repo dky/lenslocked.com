53d7f3c4a46e0cc007c2f7e4feb9c07493ea90d0
Don Ky <don.d.ky@gmail.com>
2017-07-25 17:21:18 -0400

    Add create/select user

---
+++ exp/create_user/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "pg123"
+	dbname   = "lenslocked_dev"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+ "password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec(`
+	INSERT INTO users(name, email)
+	VALUES($1, $2)`,
+		"Don Ky", "dkynyc@gmail.com")
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+}
---
+++ exp/select_user/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "pg123"
+	dbname   = "lenslocked_dev"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	var id int
+	var name, email string
+	row := db.QueryRow(`
+	SELECT id, name, email
+	FROM users
+	WHERE id=$1`, 1)
+	err = row.Scan(&id, &name, &email)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("ID:", id, "Name:", name, "Email:", email)
+
+	db.Close()
+}
