30d408dd284a79e48d81836d3fb1853df282709e
Don Ky <don.d.ky@gmail.com>
2017-07-27 10:32:39 -0400

    Query for multiple rows

---
+++ exp/query_multiple_records/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "lenslocked_dev"
+	password = "abc123"
+	dbname   = "lenslocked_dev"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	var id int
+	var name, email string
+	rows, err := db.Query(`
+	SELECT id, name, email
+	FROM users
+	WHERE email = $1
+	OR ID > $2`,
+	"d@dky.io", 3)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		rows.Scan(&id, &name, &email)
+		fmt.Println("ID:", id, "Name:", name, "Email:", email)
+	}
+
+	db.Close()
+}
+
