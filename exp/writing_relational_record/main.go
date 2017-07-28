e8c205322fce5faf54369a1e40138aac332f639b
Don Ky <don.d.ky@gmail.com>
2017-07-27 22:48:30 -0400

    Added loop to generate fake data

---
+++ exp/writing_relational_record/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "lenslocked_dev"
+	password = "abc123"
+	dbname   = "lenslocked_dev"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	var id int
+	for i := 1; i < 6; i++ {
+		//create fake data
+		userId := 1
+		if i > 3 {
+			userId = 2
+		}
+		amount := 1000 + i
+		description := fmt.Sprintf("USB-C Adapter x%d", i)
+
+		err = db.QueryRow(`
+		INSERT INTO ORDERS (user_id, amount, description)
+		VALUES ($1, $2, $3)
+		RETURNING id`,
+			userId, amount, description).Scan(&id)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Created an order with the ID:", id)
+	}
+	db.Close()
+}
