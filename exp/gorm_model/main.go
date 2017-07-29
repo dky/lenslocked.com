a23e528d03a79dea3fb2ef702ff9263d547e4d61
Don Ky <don.d.ky@gmail.com>
2017-07-29 04:13:05 -0400

    Gorm model

---
+++ exp/gorm_model/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "lenslocked_dev"
+	password = "abc123"
+	dbname   = "lenslocked_dev"
+)
+
+type User struct {
+	gorm.Model
+	Name string
+	Email string `gorm:"not null;unique_index"`
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+	db.AutoMigrate(&User{})
+}
