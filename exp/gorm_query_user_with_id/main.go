84d7de899c5bcb3c02ae8fb1b1d598df35f05701
Don Ky <don.d.ky@gmail.com>
2017-07-31 09:50:46 -0400

    Query user with specific id

---
+++ exp/gorm_query_user_with_id/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "lenslocked_dev"
+	password = "abc123"
+	dbname   = "lenslocked_dev"
+)
+
+type User struct {
+	gorm.Model
+	Name string
+	Email string `gorm:"not null;unique_index"`
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.LogMode(true)
+
+	var u User
+	//id of specific user.
+	id := 4
+
+	db.First(&u, id)
+	if db.Error != nil {
+		panic(db.Error)
+	}
+	fmt.Println(u)
+
+
+}
