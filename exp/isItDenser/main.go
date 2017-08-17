8990894bdb6b4d27009ccd25637d94696751450c
Don Ky <don.d.ky@gmail.com>
2017-08-17 16:26:23 -0400

    Add denser interface

--- exp/isItDenser/main.go
+++ exp/isItDenser/main.go
@@ -7,19 +7,47 @@ type Rock struct {
 	Volume int
 }
 
+type Geode struct {
+	Rock
+}
+
+type Dense interface {
+	Density() int
+}
+
 func (r Rock) Density() int {
+	//fmt.Println(r.Mass)
 	return r.Mass / r.Volume
 }
 
-func IsItDenser(a, b Rock) bool {
+func (g Geode) Density() int {
+	return 100
+}
+
+//This calls the Density func above.
+//func IsItDenser(a, b Rock) bool {
+func IsItDenser(a, b Dense) bool {
 	return a.Density() > b.Density()
 }
 
 func main() {
-	aDenseRock := Rock{Mass: 10, Volume: 50}
-	bDenseRock := Rock{Mass: 12, Volume: 60}
-
-	a := IsItDenser(aDenseRock, bDenseRock)
+	/*
+	aRock := Rock{Mass: 10, Volume: 50}
+	bRock := Rock{Mass: 12, Volume: 60}
+	//This dumps memory location
+	//a := aRock.Density
+	//fmt.Println(aRock.Density)
+	//fmt.Println(&a) //memory address
+	a := IsItDenser(aRock, bRock)
 	fmt.Println(a)
+	*/
+
+
+	r := Rock{10, 1}
+	g := Geode{Rock{2, 1}}
+	fmt.Println(IsItDenser(g, r))
+
+	rockDensity := r.Density
+	fmt.Println(&rockDensity)
 	
 }
