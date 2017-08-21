9e6c215a74d38aa53db6a353a011ad62c865f743
Don Ky <don.d.ky@gmail.com>
2017-08-21 15:42:42 -0400

    Add name handler for users.go

--- controllers/users.go
+++ controllers/users.go
@@ -15,6 +15,7 @@ type Users struct {
 */
 
 type SignupForm struct {
+	Name    string `schema:"name"`
 	Email    string `schema:"email"`
 	Password string `schema:"password"`
 }
@@ -40,10 +41,25 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 
 //Post signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	/*
 	var form SignupForm
 	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(w, "Email is", form.Email)
 	fmt.Fprintln(w, "Password is", form.Password)
+	*/
+	form := SignupForm{}
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	user := &models.User{
+		Name: form.Name,
+		Email: form.Email,
+	}
+
+	if err := u.UserService.Create(user); err != nil {
+		panic(err)
+	}
+	fmt.Println(w, user)
 }
